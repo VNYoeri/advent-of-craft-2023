b7c9152f2f6c562a5f2f85ec44fbe70d00d61cb4
Yoeri Van Nieuwerburg <yoeri.van.nieuwerburg@tvh.com>
2024-04-30 14:26:51 +0200

    day 6

--- exercise/java/day06/src/test/java/games/FizzBuzzTests.java
+++ exercise/java/day06/src/test/java/games/FizzBuzzTests.java
@@ -1,98 +1,68 @@
 package games;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static games.FizzBuzzTests.TestData.buzz;
+import static games.FizzBuzzTests.TestData.fizz;
+import static games.FizzBuzzTests.TestData.fizzBuzz;
+import static games.FizzBuzzTests.TestData.sameAsInput;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class FizzBuzzTests {
-    @Test
-    void returns_the_given_number_for_1() throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(1))
-                .isEqualTo("1");
-    }
 
-    @Test
-    void returns_the_given_number_for_67() throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(67))
-                .isEqualTo("67");
-    }
-
-    @Test
-    void returns_the_given_number_for_82() throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(82))
-                .isEqualTo("82");
-    }
-
-    @Test
-    void returns_Fizz_for_3() throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(3))
-                .isEqualTo("Fizz");
-    }
-
-    @Test
-    void returns_Fizz_for_66() throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(66))
-                .isEqualTo("Fizz");
-    }
-
-    @Test
-    void returns_Fizz_for_99() throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(99))
-                .isEqualTo("Fizz");
-    }
-
-    @Test
-    void returns_Buzz_for_5() throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(5))
-                .isEqualTo("Buzz");
-    }
-
-    @Test
-    void returns_Buzz_for_50() throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(50))
-                .isEqualTo("Buzz");
-    }
-
-    @Test
-    void returns_Buzz_for_85() throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(85))
-                .isEqualTo("Buzz");
-    }
-
-    @Test
-    void returns_FizzBuzz_for_15() throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(15))
-                .isEqualTo("FizzBuzz");
-    }
-
-    @Test
-    void returns_FizzBuzz_for_30() throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(30))
-                .isEqualTo("FizzBuzz");
-    }
-
-    @Test
-    void returns_FizzBuzz_for_45() throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(45))
-                .isEqualTo("FizzBuzz");
-    }
-
-    @Test
-    void throws_an_exception_for_0() {
-        assertThatThrownBy(() -> FizzBuzz.convert(0))
+    @ParameterizedTest
+    @ValueSource(ints = { -1, 0, 101 })
+    void throwsExceptionWhenInputIsInvalid(int number) {
+        assertThatThrownBy(() -> FizzBuzz.convert(number))
                 .isInstanceOf(OutOfRangeException.class);
     }
 
-    @Test
-    void throws_an_exception_for_101() {
-        assertThatThrownBy(() -> FizzBuzz.convert(101))
-                .isInstanceOf(OutOfRangeException.class);
+
+    @ParameterizedTest
+    @MethodSource
+    void returnsExpectedOutcome(TestData underTest) throws OutOfRangeException {
+        assertThat(FizzBuzz.convert(underTest.input()))
+                .isEqualTo(underTest.expectedOutput());
     }
 
-    @Test
-    void throws_an_exception_for_minus_1() {
-        assertThatThrownBy(() -> FizzBuzz.convert(-1))
-                .isInstanceOf(OutOfRangeException.class);
+    public static Stream<TestData> returnsExpectedOutcome() {
+        return Stream.of(
+                sameAsInput(1),
+                sameAsInput(67),
+                sameAsInput(82),
+                fizz(3),
+                fizz(33),
+                fizz(66),
+                fizz(99),
+                buzz(5),
+                buzz(50),
+                buzz(55),
+                buzz(85),
+                fizzBuzz(15),
+                fizzBuzz(30),
+                fizzBuzz(45)
+        );
+    }
+
+    record TestData(int input, String expectedOutput) {
+
+        static TestData sameAsInput(int input) {
+            return new TestData(input, String.valueOf(input));
+        }
+
+        static TestData fizz(int input) {
+            return new TestData(input, "Fizz");
+        }
+        static TestData buzz(int input) {
+            return new TestData(input, "Buzz");
+        }
+        static TestData fizzBuzz(int input) {
+            return new TestData(input, "FizzBuzz");
+        }
     }
 }
